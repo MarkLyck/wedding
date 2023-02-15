b8a5f71e5ef4ca9b21e0cc8b55fff02a6d5c35b8
Mark Lyck <mark.lyck@gmail.com>
2023-02-15 21:59:32 +0100

    :sparkles: our story work

--- components/OurStory/Section.tsx
+++ components/OurStory/Section.tsx
@@ -1,13 +1,36 @@
+import Image from 'next/image'
+
+import { cormorantGaramond } from '@/lib/fonts'
+
 type SectionProps = {
   title: string
+  date: string
+  imageSrc: string
   description: string
+  direction: 'left' | 'right'
 }
 
-export const Section = ({ title, description }: SectionProps) => {
+export const Section = ({ title, date, imageSrc, description, direction }: SectionProps) => {
   return (
-    <section>
-      <h3>{title}</h3>
-      <p>{description}</p>
+    <section
+      className={`flex ${
+        direction === 'left' ? 'flex-row-reverse' : 'flex-row'
+      } w-full justify-center gap-24`}
+    >
+      <div className="flex w-1/2 justify-end">
+        <Image
+          src={imageSrc}
+          alt={title}
+          height={320}
+          width={320}
+          className="h-80 rounded object-cover object-center"
+        />
+      </div>
+      <div className="flex w-1/2 flex-col gap-2">
+        <h3 className={`${cormorantGaramond.className} text-4xl`}>{title}</h3>
+        <h4 className={`${cormorantGaramond.className} text-2xl`}>{date}</h4>
+        <p className="max-w-sm">{description}</p>
+      </div>
     </section>
   )
 }
--- components/OurStory/index.tsx
+++ components/OurStory/index.tsx
@@ -4,16 +4,36 @@ import { Section } from './Section'
 
 export const OurStory = () => {
   return (
-    <div className="p-8">
+    <div className="flex flex-col gap-8 p-8">
       <SectionHeader
         title="A short timeline of our story"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm od tempor incidi dunt ut labore et dolore magna aliqua ut enim minim veniam, quis nostrud."
       />
-      <Section title="How we met" description="Lorem ipsum" />
-      <Section title="Moved to New York" description="Lorem ipsum " />
-      <Section title="Got engaged" description="Lorem ipsum " />
-      <Section title="Married in D.C" description="Lorem ipsum " />
-      <Section title="Celebrating in Copenhagen" description="Lorem ipsum " />
+      <div className="relative">
+        <div className="absolute top-4 bottom-4 left-1/2 w-0.5 bg-zinc-300" />
+        <Section
+          title="How we met"
+          date="06. 02. 2019"
+          description="We met when we were both solo-traveling in Medellin, Columbia. We happened to go on the same bus tour to El Peñón de Guatapé, a giant monolith surrounded by beautiful lakes. We met on 649 steps to the top of the monolith. The climb was followed by a tour to the nearby local town where we visited a small margarita bar."
+          imageSrc="/images/our_story/after_we_met.jpg"
+          direction="right"
+        />
+        <Section
+          title="Moved to New York"
+          date="Fall 2019"
+          description="After our trip Grace moved to New York to specialize in pediatric dentistry where Mark joined slightly after."
+          imageSrc="/images/our_story/after_we_met.jpg"
+          direction="left"
+        />
+        <Section title="Got engaged" description="Lorem ipsum " imageSrc="" direction="right" />
+        <Section title="Married in D.C" description="Lorem ipsum " imageSrc="" direction="left" />
+        <Section
+          title="Celebrating in Copenhagen"
+          description="Lorem ipsum "
+          imageSrc=""
+          direction="right"
+        />
+      </div>
     </div>
   )
 }
