2d20231beb0822b9836d56ec7457bfd6583f117d
Mark Lyck <mark.lyck@gmail.com>
2023-02-23 20:31:37 +0100

    :sparkles: middleware forwarding

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-console.log('🛑 ~ middleware')
-
 export function middleware(request: NextRequest) {
   return NextResponse.redirect(new URL('/en', request.url))
 }
