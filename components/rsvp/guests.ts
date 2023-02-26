7ae9788d0e5764b6ab4cf30f1b50a435f07b7dc6
Mark Lyck <mark.lyck@gmail.com>
2023-02-26 17:16:25 +0100

    :sparkles: finish guest list

--- components/rsvp/guests.ts
+++ components/rsvp/guests.ts
@@ -7,16 +7,6 @@ export type Guest = {
 
 export const guestList: Guest[] = [
   {
-    name: 'thomas lyck',
-    linkedGuests: ['marie lauritzen', 'anna lyck', 'liv lyck'],
-    additionalGuests: 3,
-  },
-  {
-    name: 'marie lauritzen',
-    linkedGuests: ['thomas lyck', 'anna lyck', 'liv lyck'],
-    additionalGuests: 3,
-  },
-  {
     name: 'mark lyck',
     linkedGuests: ['grace park'],
     additionalGuests: 1,
@@ -25,6 +15,27 @@ export const guestList: Guest[] = [
     name: 'grace park',
     linkedGuests: ['mark lyck'],
     additionalGuests: 1,
+  },
+  // guests
+  {
+    name: 'lone nygaarden',
+    linkedGuests: ['søren lassen'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'søren lassen',
+    linkedGuests: ['lone nygaarden'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'thomas lyck',
+    linkedGuests: ['marie lauritzen', 'anna lyck', 'liv lyck'],
+    additionalGuests: 3,
+  },
+  {
+    name: 'marie lauritzen',
+    linkedGuests: ['thomas lyck', 'anna lyck', 'liv lyck'],
+    additionalGuests: 3,
   },
   {
     name: 'søren lyck',
@@ -40,5 +51,195 @@ export const guestList: Guest[] = [
     name: 'jakob thrane',
     linkedGuests: ['helle thrane', 'emilie thrane', 'oscar thrane'],
     additionalGuests: 3,
+  },
+  {
+    name: 'jette grønkjær',
+    linkedGuests: ['peter grønkjær', 'gustav grønkjær', 'maria grønkjær'],
+    additionalGuests: 3,
+  },
+  {
+    name: 'peter grønkjær',
+    linkedGuests: ['jette grønkjær', 'gustav grønkjær', 'maria grønkjær'],
+    additionalGuests: 3,
+  },
+  {
+    name: 'vibeke harbøl',
+    linkedGuests: [],
+    additionalGuests: 0,
+  },
+  {
+    name: 'ole lyck',
+    linkedGuests: ['kirsten la cour'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'kirsten la cour',
+    linkedGuests: ['ole lyck'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'louise lauritzen',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'peter lauritzen',
+    linkedGuests: ['marianne lauritzen'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'marianne lauritzen',
+    linkedGuests: ['peter lauritzen'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'søren lyck',
+    linkedGuests: [],
+    additionalGuests: 3,
+  },
+  {
+    name: 'bent kristiansen',
+    linkedGuests: [],
+    additionalGuests: 0,
+  },
+  {
+    name: 'andreas orloff',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'sonae park',
+    linkedGuests: ['steve park'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'steve park',
+    linkedGuests: ['sonae park'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'sam park',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'susan park',
+    linkedGuests: ['david yang', 'william yang'],
+    additionalGuests: 2,
+  },
+  {
+    name: 'david yang',
+    linkedGuests: ['susan park', 'william yang'],
+    additionalGuests: 2,
+  },
+  {
+    name: 'jeannie kang',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'lucy nguy',
+    linkedGuests: ['ryan nguy'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'ryan nguy',
+    linkedGuests: ['lucy nguy'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'sarah elsamanoudi',
+    linkedGuests: ['ahad ahmed'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'ahad ahmed',
+    linkedGuests: ['sarah elsamanoudi'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'shranna lim',
+    linkedGuests: ['daniel lim'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'daniel lim',
+    linkedGuests: ['shranna lim'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'barbara weyzen',
+    linkedGuests: ['joe weyzen'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'joe weyzen',
+    linkedGuests: ['barbara weyzen'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'joe lee',
+    linkedGuests: [],
+    additionalGuests: 0,
+  },
+  {
+    name: 'diana lee',
+    linkedGuests: ['sam lee'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'sam lee',
+    linkedGuests: ['diana lee'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'minnie lee',
+    linkedGuests: ['david lee'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'david lee',
+    linkedGuests: ['minnie lee'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'alex cooke',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'tomasz rekawek',
+    linkedGuests: ['amanda oliver'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'amanda oliver',
+    linkedGuests: ['tomasz rekawek'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'falk schwiefert',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'rasa pokvytyte',
+    linkedGuests: ['vytautas pranskunas'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'rapolas micevicius',
+    linkedGuests: [],
+    additionalGuests: 1,
+  },
+  {
+    name: 'rafael anachoreta',
+    linkedGuests: ['vanessa anachoreta'],
+    additionalGuests: 1,
+  },
+  {
+    name: 'vanessa anachoreta',
+    linkedGuests: ['rafael anachoreta'],
+    additionalGuests: 1,
   },
 ].map((guest) => ({ ...guest, foodRestriction: 'none' }))
