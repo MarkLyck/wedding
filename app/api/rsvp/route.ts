480a1c2be4686b82ca18dfd686dde05e1530f39b
Mark Lyck <mark.lyck@gmail.com>
2023-02-27 22:37:53 +0100

    :lock: secure rsvp api route

---
+++ app/api/rsvp/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+
+import axios from 'axios'
+
+const AIRTABLE_API_KEY =
+  'pat9RxLTubLn628rL.c4dc6feeec3a99abdc8cd996e884df1eb6e474b75e7777e552227098cd0a83d2'
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  console.log('🔈 ~ body:', body)
+
+  const res = await axios.post(
+    'https://api.airtable.com/v0/appDPPY0ly7ZgzTd2/Table%201',
+    {
+      records: [
+        {
+          fields: body,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+
+  return NextResponse.json(res.data)
+}
