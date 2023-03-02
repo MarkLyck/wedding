3d568415c2c27926fc41defe5330bf46a720d002
Mark Lyck <mark.lyck@gmail.com>
2023-03-02 17:35:54 +0100

    :sparkles: init posthog

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -1,15 +1,18 @@
 'use client'
 
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
+
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import { Toaster } from '@/components/ui/toaster'
-
-import '@/lib/analytics'
-
+import { analyticsInit } from '@/lib/analytics'
 import { queryClient } from '@/lib/queryClient'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    analyticsInit()
+  }, [])
+
   return (
     <>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
